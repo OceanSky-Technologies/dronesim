79018e4268a044cdd3f2989008ac296d0e60b2c1
Martin Schlodinski <martin.schlodinski@moia.io>
2024-10-03 23:48:48 +0200

    add skywinger model

--- buildAirSim.cmd
+++ buildAirSim.cmd
@@ -9,8 +9,8 @@ if %ERRORLEVEL% neq 0 (
 set plugin_folder=.\Unreal\Dronesim\Plugins\AirSim
 
 REM Check if the folder exists
-if exist "%plugin_folder%" (
-    rmdir /s /q "%plugin_folder%"
-)
+REM if exist "%plugin_folder%" (
+REM     rmdir /s /q "%plugin_folder%"
+REM )
 
 robocopy .\Colosseum\Unreal\Plugins\AirSim "%plugin_folder%" /E
